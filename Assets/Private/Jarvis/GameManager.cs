e450f07db6493d4f8c0a1b4e0265d3677772f293
BigJDiesel20 <BigJDiesel20@aol.com>
2020-04-08 19:02:16 -0500

    Update

---
+++ Assets/Private/Jarvis/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    
+    string currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadScene(string sceneName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        ao.completed = 
+            
+
+    }
+
+
+}
---
+++ Assets/Private/Jarvis/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = (T)this;
+        }
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
