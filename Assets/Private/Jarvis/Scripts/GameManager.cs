7b2946a844a7d0b3b77dab11b0b69bea6ba604a1
BigJDiesel20 <BigJDiesel20@aol.com>
2020-04-09 17:25:03 -0500

    New Commit

---
+++ Assets/Private/Jarvis/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    
+    string currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadScene(string sceneName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        
+            
+
+    }
+
+    void UnloadScene(string sceneName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+
+
+
+}
