f8c2d7019cf8293de7d8820c0c594bf64e516f9a
Daniel Heffner <cdanielheffner@gmail.com>
2020-04-28 22:14:58 -0500

    timer works, full HUD functional

---
+++ Assets/HUD Scripts/RunTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTimer : MonoBehaviour
+{
+
+    public Timer Timer;
+
+    void Start()
+    {
+        Timer.IsActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            Debug.Log("RESET TIMER!!!");
+            Timer.ResetCount(true);
+        }
+        else
+        {
+            Timer.Counting();
+        }
+    }
+}
